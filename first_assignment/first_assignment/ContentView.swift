ed2182b343515180f901c2f9c2b638de925a51f4
soseoyo12 <songsy0612@gmail.com>
2025-07-01 19:03:58 +0900

    init commit

---
+++ first_assignment/first_assignment/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  first_assignment
+//
+//  Created by soseoyo_12 on 7/1/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ first_assignment/first_assignment/first_assignmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_assignmentApp.swift
+//  first_assignment
+//
+//  Created by soseoyo_12 on 7/1/25.
+//
+
+import SwiftUI
+
+@main
+struct first_assignmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
